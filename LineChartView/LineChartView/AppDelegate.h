956f236d99a3a9ea6cf80e757c0a6de3f7732b01
tangzhentao <tangzhenta0@126.com>
2017-03-15 16:48:11 +0800

    init

---
+++ LineChartView/LineChartView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LineChartView
+//
+//  Created by tang on 2017/3/15.
+//  Copyright © 2017年 tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
